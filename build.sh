4e87fe6b6b33e6a76668ff272dbe7292481e4110
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2021-11-30 18:27:42 -0300

    Updated go version and build

--- build.sh
+++ build.sh
@@ -17,8 +17,8 @@ then
   bash create-tar-gz.sh
   ls
 
-  #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/crio/latest/crio-$ftp_version.linux-ppc64le.tar.gz"
-  #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/crio/latest crio-$github_version.linux-ppc64le.tar.gz"
+  lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/crio/latest/crio-$ftp_version.linux-ppc64le.tar.gz"
+  lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/crio/latest crio-$github_version.linux-ppc64le.tar.gz"
   lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/crio crio-$github_version.linux-ppc64le.tar.gz"
   #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/crio/crio-$del_version.linux-ppc64le.tar.gz"
 fi
